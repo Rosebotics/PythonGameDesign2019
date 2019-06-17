8618a0c98b1dd2b1f3e7f632645ec6018d62fc51
mobacdesign <fisherds@mobacdesign>
2019-06-17 16:15:18 -0400

    first day

--- camp/Marika/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
+++ camp/Marika/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
@@ -11,3 +11,16 @@ print('3 + 9', 'versus', 3 + 9)
 # TODO:    2. Two big numbers, followed by their sum.
 # TODO:    3. The result of 3,607 multiplied by 34,227.  (Hint: the result is interesting.)
 # TODO:    4. Anything else you like!
+
+print()
+print('Hello, Bob')
+print('I do not know who that is')
+print('123 + 1234 =' , 123 + 1234)
+print(3607 * 34227)
+print('Anything else you like!')
+print('Hi, Amanda')
+print('Hi, Neil')
+print('Hi, Caleb')
+print('Hi, Dr. Fisher')
+print('Hi, Becca')
+print('This actually is not that hard')
--- camp/Marika/Day 1 - The Game Loop, Colors, Drawing and Animation/06-InteractingWithAPlayer.py
+++ camp/Marika/Day 1 - The Game Loop, Colors, Drawing and Animation/06-InteractingWithAPlayer.py
@@ -1,1 +1,47 @@
-# TODO: Copy all of your   05-Animation.py   program and put it below this comment.
+# My first Pygame program.
+# Authors: Many people and Marika
+
+import pygame
+import sys
+from pygame.locals import *
+
+screenSize = (640, 480)
+backgroundColor = (0, 0, 0)
+circleColor = (140, 100, 255)
+rectColor1 = (255, 100, 120)
+rectColor2 = (100, 120, 255)
+circleRadius =  20
+
+rectY1 = 100
+rectY2 = 100
+rectSpeed = 5
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
+
+while True:
+    clock.tick(60)
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT:
+            sys.exit()
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP]:
+            rectY1 = rectY1 - 5
+        if pressed_keys[K_DOWN]:
+            rectY1 = rectY1 + 5
+        if pressed_keys[K_w]
+            rectY2 = rectY2 -5
+        if pressed_keys[K_s]
+
+
+    screen.fill((backgroundColor))
+
+
+
+    pygame.draw.circle(screen, (circleColor), (320, 150), circleRadius)
+    pygame.draw.rect(screen, (rectColor1), (600, rectY1, 20, 75))
+    pygame.draw.rect(screen, (rectColor2), (30, rectY2, 20, 75))
+
+
+    pygame.display.update()
+
