7bdc38370b2e137809e504db24e7ccb9107de7d2
mobacdesign <fisherds@mobacdesign.com>
2019-06-17 16:19:24 -0400

    first day

--- camp/Camden/Day 1 - The Game Loop, Colors, Drawing and Animation/04-Drawing.py
+++ camp/Camden/Day 1 - The Game Loop, Colors, Drawing and Animation/04-Drawing.py
@@ -1,3 +1,4 @@
+
 # TODO: Copy all of your   03-Colors.py   program and put it below this comment.
 # TODO    One way to do so is:
 # TODO      1. Inside  03-Colors.py,  do:
@@ -6,3 +7,33 @@
 # TODO      2. Inside this file:
 # TODO           -- Click below this comment, then
 # TODO           -- Control-V (to PASTE the copied code into this file.
+
+
+import pygame
+import sys
+
+screenSize = (640, 480)
+backgroundColor = (255, 0, 0)
+
+screenSize = (640, 480)
+backgroundColor = (234, 19, 210)
+circleColor = (0, 129, 9)
+circleRadius = 20
+
+
+pygame.init
+screen = pygame.display.set_mode(screenSize)
+while True:
+     for event in pygame.event.get():
+         if event.type == pygame.QUIT:
+            sys.exit()
+
+
+     # fill background color
+     screen.fill((255, 203, 10))
+
+     pygame.draw.circle(screen, circleColor , (300, 150), circleRadius)
+     pygame.draw.rect(screen, (255, 255, 0), (600, 100, 20, 75))
+
+     pygame.display.update()
+
