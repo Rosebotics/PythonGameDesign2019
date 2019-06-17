d4b022328dc23028a2babde6061e2ae871720ee3
mobacdesign <fisherds@mobacdesign.com>
2019-06-17 13:06:23 -0400

    xxx

--- camp/DaveM/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
+++ camp/DaveM/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
@@ -1,4 +1,4 @@
-# Authors: David Mutchler, Dave Fisher, and many others before them.
+# Authors: David Mutchler, Dave Fisher, and many others before them.xxx
 
 print('Hello, World')
 print('hi there')
