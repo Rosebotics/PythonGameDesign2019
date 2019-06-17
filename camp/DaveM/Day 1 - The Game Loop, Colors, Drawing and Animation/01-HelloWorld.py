b5a335d8a2d4366679a97a68791b466fcfa8c240
mobacdesign <fisherds@mobacdesign.com>
2019-06-17 16:22:35 -0400

    xxx

--- camp/DaveM/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
+++ camp/DaveM/Day 1 - The Game Loop, Colors, Drawing and Animation/01-HelloWorld.py
@@ -10,6 +10,11 @@ print('223-126=97')
 print('3,607*34,227=',3607*34227)
 print('Why you bully me because someone touched my spaget')
 
+print('Hello, Donnie!  and Andrew!  And Alvina')
+print(5678 + 122345678090)
+print(3607 * 34227)
+print(590 / 436)
+
 # TODO:  After we talk together about the above, add PRINT statements that print:
 # TODO:    1. A Hello message to a friend.
 # TODO:    2. Two big numbers, followed by their sum.
