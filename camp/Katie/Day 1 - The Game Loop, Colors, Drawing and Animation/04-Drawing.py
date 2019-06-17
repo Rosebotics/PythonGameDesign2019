565e23943a4ac4b25097393087890f1c00d97347
mobacdesign <fisherds+mobacdesign@gmail.com>
2019-06-17 16:19:04 -0400

    first day

--- camp/Katie/Day 1 - The Game Loop, Colors, Drawing and Animation/04-Drawing.py
+++ camp/Katie/Day 1 - The Game Loop, Colors, Drawing and Animation/04-Drawing.py
@@ -6,3 +6,25 @@
 # TODO      2. Inside this file:
 # TODO           -- Click below this comment, then
 # TODO           -- Control-V (to PASTE the copied code into this file.
+
+import pygame
+import sys
+
+screenSize = (640, 480)
+backgroundColor = (100, 255, 200)
+circleColor = (255, 255, 255)
+circleRadius = 20
+
+pygame.init()
+screen = pygame.display.set_mode((screenSize))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill((backgroundColor))
+
+    pygame.draw.circle(screen, circleColor, (300, 150), circleRadius)
+    pygame.draw.rect(screen, (0, 0, 0), (600, 100, 20, 75))
+
+    pygame.display.update()
