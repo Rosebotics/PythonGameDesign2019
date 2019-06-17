ee52eae429ca005f8f00ca7515cccd59a8087659
mobacdesign <fisherds@mobacdesign.com>
2019-06-17 16:10:38 -0400

    xxx

--- camp/Aidan/Day 1 - The Game Loop, Colors, Drawing and Animation/02-TheGameLoop.py
+++ camp/Aidan/Day 1 - The Game Loop, Colors, Drawing and Animation/02-TheGameLoop.py
@@ -1,16 +1,36 @@
 # My first Pygame program.
-# Authors: Many people and <PUT-YOUR-NAME-HERE>
+# Authors: Many people and Aidan
 
 import pygame
 import sys
 
-
+pygame.init()
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
     pygame.display.update()
 
 
-# TODO: With your instructor's help (live coding):
-#   - Put your name as an author.
+# TODO: With your instructor's help (live coding):#   - Put your name as an author.
+#   - Make the game loop be able to stop more gracefully.
+# My first Pygame program.
+# Authors: Many people and Aidan
+
+import pygame
+import sys
+
+pygame.init()
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    pygame.display.update()
+
+
+# TODO: With your instructor's help (live coding):#   - Put your name as an author.
 #   - Make the game loop be able to stop more gracefully.
--- camp/Aidan/Day 1 - The Game Loop, Colors, Drawing and Animation/05-Animation.py
+++ camp/Aidan/Day 1 - The Game Loop, Colors, Drawing and Animation/05-Animation.py
@@ -1,1 +1,34 @@
+
+# My first Pygame program.
+# Authors: Many people and Aidan
+
+import pygame
+import sys
+import time
+
+backgroundColor = (50, 60, 123.456789)
+
+white = (255, 255, 255)
+circleLocation = (300, 150)
+pygame.init()
+
+circleRaidous = 100
+
+
+
+rectSpeed = 1
+screen = pygame.display.set_mode((640, 480))
+while True:
+    time.sleep(0.09)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    circleRaidous = circleRaidous + 1
+    screen.fill(backgroundColor)
+    pygame.draw.circle(screen, white, circleLocation, circleRaidous)
+
+
+    pygame.display.update()
+
+
 # TODO: Copy all of your   04-Drawing.py   program and put it below this comment.
